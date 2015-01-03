d7ac7698f272f386cdeb5fa65eb3fdfe4bc0054f
becauseimgray <otakugray@gmail.com>
2015-01-03 01:41:50 -0700

    Added Scores, Menu functionality, and Enemy respawning

--- main.lua
+++ main.lua
@@ -2,15 +2,56 @@
 --Created by Gray
 --Due Feb 1st
 --www.github.com/becauseimgray
+--Thank you to Pix for creating bgmusic
+--http://www.sakari-infinity.net/author/pix/
 
 require("AnAL")
 require("BoundingBox")
+Menu = require("menu")
+require ('sounds')
 
 function love.load()
+
+	--load soundtrack
+	clicked = love.audio.newSource("sounds/clicked.wav", "static")
+	bgm = love.audio.play("sounds/bgsong.mp3", "stream", true)
+	love.audio.stop(bgm)
+
+	--create font 
+	font = love.graphics.setNewFont( "fonts/LCD_Solid.ttf", 80 )
+	default = love.graphics.setNewFont( "fonts/LCD_Solid.ttf", 10 )
+
+
+	--create game bool
+	gameStart = false
+
+	--create menu
+	menu = Menu.new()
+   		menu:addItem{
+     	name = 'Start Game',
+      	action = function()
+      		if gameStart == false then
+      			clicked:play()
+      			love.audio.play(bgm)
+      		end
+        gameStart = true
+    	end
+   }
+   menu:addItem{
+      	name = 'Quit',
+      	action = function()
+      	gameStart = false
+      	love.event.quit()
+      	end
+   }
+
+
+    
 
 	--get window dimensions
 	winw = love.window.getWidth()
 	winh = love.window.getHeight()
+
 	--create player
 	player = {}
 	player.image = love.graphics.newImage('images/player.png')
@@ -23,6 +64,7 @@ function love.load()
 
 	--create enemies 
 	enemies = {}
+
 	--set variables for enemies
 	for i = 0, 7 do
 		enemy = {}
@@ -30,16 +72,18 @@ function love.load()
 		enemy.w = 32
 		enemy.h = 32
 
-	--set enemies location
-
 	--how far from (0, 0) they spawn
 		enemy.x = i * (enemy.w + 60) + 100
 		enemy.y = enemy.h + 100
 	--enemy's current speed
 		enemy.speed = 60
 		table.insert(enemies, enemy)
+
 	--creating a boolean to tell which direction we are currently moving
 		enemy.right = true
+	end
+
+
 
 	--creating collision rectangle for left side
 	colrectL = {}
@@ -58,30 +102,41 @@ function love.load()
 	colrectR.mode = "fill"
 
 	--create/load background image
-
 	background = love.graphics.newImage('images/bg.png')
+	--Bullet Image
+	bulletimg = love.graphics.newImage('images/bullet.png')
 
 	--bullet init here
 	canShoot = true
-	canShootTimerMax = 0.9
+	canShootTimerMax = 0.1 --0.9 = perfect speed || lower speeds = faster shooting
+	shootSpeed = 100
 	canShootTimer = canShootTimerMax
-
-	--Bullet Image
-	bulletimg = love.graphics.newImage('images/bullet.png')
 
 	--Bullet entity
 	bullets = {}
 
-	--isalive?
-	isAlive = true
+	--Later on, we'll use this to see if the player is alive when hit with an enemy bullet
+		--isAlive = true
 
 	--scoring system
 	score = 0
 
 end
 
+function love.keypressed(key)
+	--keys for menu
+   menu:keypressed(key)
+end
 
 function love.update(dt)
+	--update menu
+	menu:update(dt)
+
+	--update music if playing game
+	if gameStart == true then
+		love.audio.update()
+	end
+
 	--canshoot?
 	canShootTimer = canShootTimer - (1 * dt)
 	if canShootTimer < 0 then
@@ -89,17 +144,20 @@ function love.update(dt)
 	end
 
 
-    --boundaries and movement for player
+   --boundaries and movement for player
 	if love.keyboard.isDown("right") and player.x + player.w <= winw then
 		player.x = player.x + player.speed * dt
 	end
 
     if love.keyboard.isDown("left") and player.x >= 0 then
         player.x = player.x - player.speed * dt;
-    end
+end
+
+
 
     --controls for bullet shooting 
     if love.keyboard.isDown(' ', 'z') and canShoot then
+
 	-- Create some bullets
 	newBullet = { x = player.x + (player.image:getWidth()/2), y = player.y, img = bulletImg }
 	table.insert(bullets, newBullet)
@@ -107,16 +165,9 @@ function love.update(dt)
 	canShootTimer = canShootTimerMax
 	end
 
-	-- update the positions of bullets
-	for i, bullet in ipairs(bullets) do
-		bullet.y = bullet.y - (250 * dt)
 
-  		if bullet.y < 0 then -- remove bullets when they pass off the screen
-			table.remove(bullets, i)
-		end
-	end
 
-    --boundaries and movement for enemies
+	--boundaries and movement for enemies
 
 	--starting enemies loop
     for i,v in ipairs(enemies) do
@@ -139,7 +190,17 @@ function love.update(dt)
 		if enemy.right == false then
 			v.x = v.x - enemy.speed * dt
 		end
-    end
+
+
+
+	-- update the positions of bullets
+	for i, bullet in ipairs(bullets) do
+		bullet.y = bullet.y - (shootSpeed * dt)
+
+  		if bullet.y < 0 then -- remove bullets when they pass off the screen
+			table.remove(bullets, i)
+		end
+	end
 
     	--bullet collision logic loop
     for i, enemy in ipairs(enemies) do
@@ -151,12 +212,42 @@ function love.update(dt)
 			end
 		end
 	end
+	if not next(enemies) then
+		for j, bullet in ipairs(bullets) do
+			table.remove(bullets, j)
+		end
+				for i = 0, 7 do
+					enemy = {}
+					enemy.image = love.graphics.newImage('images/enemy.png')
+					enemy.w = 32
+					enemy.h = 32
+
+
+		--how far from (0, 0) they spawn
+					enemy.x = i * (enemy.w + 60) + 100
+					enemy.y = enemy.h + 100
+		--enemy's current speed
+					enemy.speed = 60
+					table.insert(enemies, enemy)
+		--creating a boolean to tell which direction we are currently moving
+    				enemy.right = true
+				end
+			
+
+	end
+
 end
 end
+
+
 
 
 function love.draw()
-
+	
+if gameStart == true then
+	love.graphics.setFont( font )
+	love.graphics.setColor(255,255,255,255)
+	
 	--drawing background
 		love.graphics.draw(background)
 	
@@ -179,5 +270,16 @@ function love.draw()
     	love.graphics.draw(enemy.image, v.x, v.y, v.width, v.height)
 	end
 
+	love.graphics.print("Score:" .. score, 10, 5)
 
 end
+	if gameStart == false then
+		love.graphics.setFont( default )
+		love.graphics.draw(background)
+		--draw menu
+		menu:draw(winw / 2 - 50, winh / 2)
+	end
+end
+
+
+
---
+++ menu.lua
@@ -0,0 +1,64 @@
+--[[
+Simple Menu Library
+by nkorth
+
+Requires: love2d
+Recommended: hump.gamestate
+
+Public Domain - feel free to hack and redistribute this as much as you want.
+]]--
+return {
+	new = function()
+		return {
+			items = {},
+			selected = 1,
+			animOffset = 0,
+			addItem = function(self, item)
+				table.insert(self.items, item)
+			end,
+			update = function(self, dt)
+				self.animOffset = self.animOffset / (1 + dt*10)
+			end,
+			draw = function(self, x, y)
+				local height = 20
+				local width = 80
+				
+				love.graphics.setColor(255, 255, 255, 128)
+				love.graphics.rectangle('fill', x, y + height*(self.selected-1) + (self.animOffset * height), width, height)
+				
+				for i, item in ipairs(self.items) do
+					if self.selected == i then
+						--change these graphics to change "background effect" of menu
+						love.graphics.setColor(255, 255, 255, 128)
+					else
+						love.graphics.setColor(255, 255, 255, 128)
+					end
+					love.graphics.print(item.name, x + 5, y + height*(i-1) + 5)
+				end
+			end,
+			keypressed = function(self, key)
+				if key == 'up' then
+					if self.selected > 1 then
+						self.selected = self.selected - 1
+						self.animOffset = self.animOffset + 1
+					else
+						self.selected = #self.items
+						self.animOffset = self.animOffset - (#self.items-1)
+					end
+				elseif key == 'down' then
+					if self.selected < #self.items then
+						self.selected = self.selected + 1
+						self.animOffset = self.animOffset - 1
+					else
+						self.selected = 1
+						self.animOffset = self.animOffset + (#self.items-1)
+					end
+				elseif key == 'z' then
+					if self.items[self.selected].action then
+						self.items[self.selected]:action()
+					end
+				end
+			end
+		}
+	end
+}
